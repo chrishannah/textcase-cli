4328db224c684f8846676fa4b107e92dc2d08fa5
Christopher Hannah <me@chrishannah.me>
2023-08-20 23:40:08 +0100

    restructure to work with swift package manager

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "textcase",
+    platforms: [.macOS(.v13)],
+    products: [
+        .library(name: "TextCaseKit", targets: ["TextCaseKit"]),
+        .executable(name: "TextCase", targets: ["TextCase"]),
+    ],
+    targets: [
+        .executableTarget(
+            name: "TextCase",
+            dependencies: ["TextCaseKit"]),
+        .target(
+            name: "TextCaseKit"),
+        .testTarget(
+            name: "TextCaseKitTests",
+            dependencies: ["TextCaseKit"]),
+    ]
+)
