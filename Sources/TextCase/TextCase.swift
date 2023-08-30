1d83c4912e74f389f8bf1b374dd448cf53f35e46
Christopher Hannah <me@chrishannah.me>
2023-08-30 16:13:33 +0100

    add version option

--- Sources/TextCase/TextCase.swift
+++ Sources/TextCase/TextCase.swift
@@ -4,6 +4,7 @@ import ArgumentParser
 struct TextCase: ParsableCommand {
     static var configuration = CommandConfiguration(
         abstract: "A text transformation tool.",
+        version: "1.2",
         subcommands: [Format.self, ListFormats.self],
         defaultSubcommand: Format.self
     )
