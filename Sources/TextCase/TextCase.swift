dee0b8a4672bf1d65e9f5851b3295cf1ffa8354c
Christopher Hannah <me@chrishannah.me>
2023-10-31 17:37:28 +0000

    fix command name

--- Sources/TextCase/TextCase.swift
+++ Sources/TextCase/TextCase.swift
@@ -3,6 +3,7 @@ import ArgumentParser
 @main
 struct TextCase: ParsableCommand {
     static var configuration = CommandConfiguration(
+        commandName: "textcase",
         abstract: "A text transformation tool.",
         version: "1.3",
         subcommands: [Format.self, ListFormats.self],
