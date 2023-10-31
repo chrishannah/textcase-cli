df8cdf36c881470d9f59f35d17f90ced5e51f7ae
Christopher Hannah <me@chrishannah.me>
2023-10-31 17:47:09 +0000

    increase version to 1.4

--- Sources/TextCase/TextCase.swift
+++ Sources/TextCase/TextCase.swift
@@ -5,7 +5,7 @@ struct TextCase: ParsableCommand {
     static var configuration = CommandConfiguration(
         commandName: "textcase",
         abstract: "A text transformation tool.",
-        version: "1.3",
+        version: "1.4",
         subcommands: [Format.self, ListFormats.self],
         defaultSubcommand: Format.self
     )
